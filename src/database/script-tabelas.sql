62c6925907559020344dfaa8b0ee977304f0da24
leticia-rsm <leticia.mendes@sptech.school>
2024-11-04 20:09:48 -0300

    Alteracao no script do Banco de Dados

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,5 +1,11 @@
+DROP DATABASE IF EXISTS projeto_individual;
 CREATE DATABASE projeto_individual;
 USE projeto_individual;
+
+CREATE TABLE escolha (
+idEscolha INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45)
+);
 
 CREATE TABLE usuario (
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
@@ -8,15 +14,10 @@ dtNasc DATE,
 email VARCHAR(45),
 senha VARCHAR(45),
 dispositivo VARCHAR(20),
-CONSTRAINT chkDispositivo CHECK (dispositivo IN('playstation 5', 'playstation 4', 'playstation 3', 'notebook/desktop', 'outro')),
+CONSTRAINT chkDispositivo CHECK (dispositivo IN('ps5', 'ps4', 'ps3', 'pc', 'outro')),
 fkEscolha INT,
 CONSTRAINT fkEscolhaUsuario FOREIGN KEY (fkEscolha)
 	REFERENCES escolha(idEscolha)
-);
-
-CREATE TABLE escolha (
-idEscolha INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(45)
 );
 
 SHOW TABLES;
