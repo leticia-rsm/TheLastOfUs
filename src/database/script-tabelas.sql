b76b66244518563897570804349f2438377e825f
leticia-rsm <leticia.mendes@sptech.school>
2024-11-07 13:41:36 -0300

    Script Banco de Dados

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -2,11 +2,6 @@ DROP DATABASE IF EXISTS projeto_individual;
 CREATE DATABASE projeto_individual;
 USE projeto_individual;
 
-CREATE TABLE escolha (
-idEscolha INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(45)
-);
-
 CREATE TABLE usuario (
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nickname VARCHAR(45),
@@ -14,11 +9,31 @@ dtNasc DATE,
 email VARCHAR(45),
 senha VARCHAR(45),
 dispositivo VARCHAR(20),
-CONSTRAINT chkDispositivo CHECK (dispositivo IN('ps5', 'ps4', 'ps3', 'pc', 'outro')),
-fkEscolha INT,
-CONSTRAINT fkEscolhaUsuario FOREIGN KEY (fkEscolha)
-	REFERENCES escolha(idEscolha)
+CONSTRAINT chkDispositivo CHECK (dispositivo IN('ps5', 'ps4', 'ps3', 'pc', 'outro'))
 );
+
+CREATE TABLE pesquisa (
+idPesquisa INT PRIMARY KEY AUTO_INCREMENT,
+descricao VARCHAR(100)
+)AUTO_INCREMENT = 1000;
+
+CREATE TABLE resposta (
+idResposta INT PRIMARY KEY AUTO_INCREMENT,
+alternativa CHAR(1),
+fkPesquisa INT,
+CONSTRAINT fkPesquisaResposta FOREIGN KEY (fkPesquisa)
+	REFERENCES pesquisa(idPesquisa)
+)AUTO_INCREMENT = 2000;
+
+CREATE TABLE grafico (
+idGrafico INT PRIMARY KEY AUTO_INCREMENT,
+fkUsuario INT,
+CONSTRAINT fkUsuarioGrafico FOREIGN KEY (fkUsuario)
+	REFERENCES usuario(idUsuario),
+fkResposta INT,
+CONSTRAINT fkRespostaGrafico FOREIGN KEY (fkResposta)
+	REFERENCES resposta(idResposta)
+)AUTO_INCREMENT = 3000;
 
 SHOW TABLES;
 
