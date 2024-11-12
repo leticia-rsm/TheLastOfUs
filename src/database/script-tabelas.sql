2d5f597de246f76ef441a3c86d760ac0c299679f
leticia-rsm <leticia.mendes@sptech.school>
2024-11-12 14:17:57 -0300

    Script e Funções da Dash

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -1,6 +1,11 @@
-DROP DATABASE IF EXISTS projeto_individual;
 CREATE DATABASE projeto_individual;
 USE projeto_individual;
+
+CREATE TABLE siteFooter(
+idSite INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+qtdClick INT
+);
 
 CREATE TABLE usuario (
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
@@ -15,28 +20,24 @@ CONSTRAINT chkDispositivo CHECK (dispositivo IN('ps5', 'ps4', 'ps3', 'pc', 'outr
 CREATE TABLE pesquisa (
 idPesquisa INT PRIMARY KEY AUTO_INCREMENT,
 descricao VARCHAR(100)
-)AUTO_INCREMENT = 1000;
+);
 
 CREATE TABLE resposta (
 idResposta INT PRIMARY KEY AUTO_INCREMENT,
-alternativa CHAR(1),
+r_alternativa CHAR(1),
+CONSTRAINT chkr_alternativa CHECK (r_alternativa IN('a', 'b', 'c', 'd')),
 fkPesquisa INT,
 CONSTRAINT fkPesquisaResposta FOREIGN KEY (fkPesquisa)
 	REFERENCES pesquisa(idPesquisa)
-)AUTO_INCREMENT = 2000;
+)AUTO_INCREMENT = 1000;
 
-CREATE TABLE grafico (
-idGrafico INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE alternativa (
+idAlternativa INT AUTO_INCREMENT,
 fkUsuario INT,
-CONSTRAINT fkUsuarioGrafico FOREIGN KEY (fkUsuario)
+CONSTRAINT fkUsuarioAlternativa FOREIGN KEY (fkUsuario)
 	REFERENCES usuario(idUsuario),
 fkResposta INT,
-CONSTRAINT fkRespostaGrafico FOREIGN KEY (fkResposta)
-	REFERENCES resposta(idResposta)
+CONSTRAINT fkRespostaAlternativa FOREIGN KEY (fkResposta)
+	REFERENCES resposta(idResposta),
+PRIMARY KEY (idAlternativa, fkUsuario, fkResposta)
 )AUTO_INCREMENT = 3000;
-
-SHOW TABLES;
-
-DESC TABLE usuario;
-
-SELECT * FROM usuario;
