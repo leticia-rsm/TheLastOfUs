f769919fc9d338ab486da88e84effd6073fbc4cf
leticia-rsm <leticia.mendes@sptech.school>
2024-11-28 02:02:42 -0300

    Atualização do script mysql e modelagem atualizada

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -22,25 +22,84 @@ idPesquisa INT PRIMARY KEY AUTO_INCREMENT,
 descricao VARCHAR(100)
 );
 
-CREATE TABLE resposta (
-idResposta INT PRIMARY KEY AUTO_INCREMENT,
-r_alternativa CHAR(1),
-CONSTRAINT chkr_alternativa CHECK (r_alternativa IN('A', 'B', 'C', 'D')),
+CREATE TABLE alternativa (
+idAlternativa INT PRIMARY KEY AUTO_INCREMENT,
+caractere CHAR(1),
+CONSTRAINT chkr_caractere CHECK (caractere IN('A', 'B', 'C', 'D')),
 fkPesquisa INT,
-CONSTRAINT fkPesquisaResposta FOREIGN KEY (fkPesquisa)
+CONSTRAINT fkPesquisaAlternativa FOREIGN KEY (fkPesquisa)
 	REFERENCES pesquisa(idPesquisa)
 );
 
-CREATE TABLE alternativa (
-idAlternativa INT AUTO_INCREMENT,
+CREATE TABLE resposta (
+idResposta INT AUTO_INCREMENT,
 fkUsuario INT,
-CONSTRAINT fkUsuarioAlternativa FOREIGN KEY (fkUsuario)
+CONSTRAINT fkUsuarioResposta FOREIGN KEY (fkUsuario)
 	REFERENCES usuario(idUsuario),
-fkResposta INT,
-CONSTRAINT fkRespostaAlternativa FOREIGN KEY (fkResposta)
-	REFERENCES resposta(idResposta),
-PRIMARY KEY (idAlternativa, fkUsuario, fkResposta)
+fkAlternativa INT,
+CONSTRAINT fkAlternativaResposta FOREIGN KEY (fkAlternativa)
+	REFERENCES alternativa(idAlternativa),
+PRIMARY KEY (idResposta, fkUsuario, fkAlternativa)
 );
+
+SELECT * FROM siteFooter;
+SELECT * FROM usuario;
+SELECT * FROM pesquisa;
+SELECT * FROM resposta;
+SELECT * FROM alternativa;
+
+-- INSERTS
+INSERT INTO siteFooter (nome) VALUES
+('PS'),
+('PS'),
+('PS'),
+('HBO'),
+('HBO');
+
+INSERT INTO usuario (nickname, dtNasc, email, senha, dispositivo) VALUES
+('User1', '1996-01-01', 'user1@example.com', 'senha123', 'ps5'),
+('User2', '1995-05-05', 'user2@example.com', 'senha456', 'pc'),
+('User3', '1992-03-12', 'user3@example.com', 'senha789', 'ps4'),
+('User4', '1996-07-22', 'user4@example.com', 'senha101', 'outro'),
+('User5', '2004-11-11', 'user5@example.com', 'senha102', 'ps5'),
+('User6', '2004-09-09', 'user6@example.com', 'senha103', 'ps3'),
+('User7', '2000-02-20', 'user7@example.com', 'senha104', 'pc'),
+('User8', '2000-12-12', 'user8@example.com', 'senha105', 'outro'),
+('User9', '1996-08-18', 'user9@example.com', 'senha106', 'ps4'),
+('User10', '2004-06-06', 'user10@example.com', 'senha107', 'ps3');
+
+INSERT INTO pesquisa (descricao) VALUES
+('Sua escolha'),
+('Porque você escolheu essa opção?'),
+('Como você se sentiu ao fazer essa escolha?'),
+('Qual você acha que seria o impacto dessa escolha no futuro dos personagens?');
+
+
+INSERT INTO alternativa (caractere, fkPesquisa) VALUES
+('A', 1),
+('B', 1),
+-- Pergunta 2
+('A', 2),
+('B', 2),
+('C', 2),
+('D', 2),
+-- Pergunta 3
+('A', 3),
+('B', 3),
+('C', 3),
+('D', 3),
+-- Pergunta 4
+('A', 4),
+('B', 4),
+('C', 4),
+('D', 4);
+
+INSERT INTO resposta (fkUsuario, fkAlternativa) VALUES
+(1, 1),
+(1, 2),
+(1, 4),
+(1, 9),
+(1, 14);
 
 -- SELECTS
 
@@ -49,41 +108,41 @@ SELECT nome, COUNT(idSite) FROM siteFooter
 	GROUP BY nome;
 
 -- SELECT AUXILIAR (GERAL)
-SELECT * FROM pesquisa JOIN resposta ON fkPesquisa = idPesquisa JOIN alternativa ON fkResposta = idResposta;
+SELECT * FROM pesquisa JOIN alternativa ON fkPesquisa = idPesquisa JOIN resposta ON fkAlternativa = idAlternativa;
 
 -- QUESTIONÁRIO
 -- VOTOS
-SELECT resposta.r_alternativa, COUNT(alternativa.fkResposta) FROM usuario JOIN alternativa 
-	ON usuario.idUsuario = alternativa.fkUsuario JOIN resposta 
-    ON resposta.idResposta = alternativa.fkResposta
+SELECT alternativa.caractere, COUNT(resposta.fkAlternativa) AS 'qtd' FROM usuario JOIN resposta 
+	ON usuario.idUsuario = resposta.fkUsuario JOIN alternativa 
+    ON alternativa.idAlternativa = resposta.fkAlternativa
     WHERE fkPesquisa = 1
-	GROUP BY resposta.r_alternativa;
+	GROUP BY alternativa.caractere;
 
 -- PERGUNTA 1
-SELECT resposta.r_alternativa, COUNT(alternativa.fkResposta) FROM usuario JOIN alternativa 
-	ON usuario.idUsuario = alternativa.fkUsuario JOIN resposta 
-    ON resposta.idResposta = alternativa.fkResposta
+SELECT alternativa.caractere, COUNT(resposta.fkAlternativa) FROM usuario JOIN resposta 
+	ON usuario.idUsuario = resposta.fkUsuario JOIN alternativa 
+    ON alternativa.idAlternativa = resposta.fkAlternativa
     WHERE fkPesquisa = 2
-	GROUP BY resposta.r_alternativa;
+	GROUP BY alternativa.caractere;
 
 -- PERGUNTA 2    
-SELECT resposta.r_alternativa, COUNT(alternativa.fkResposta) FROM usuario JOIN alternativa 
-	ON usuario.idUsuario = alternativa.fkUsuario JOIN resposta 
-    ON resposta.idResposta = alternativa.fkResposta
+SELECT alternativa.caractere, COUNT(resposta.fkAlternativa) FROM usuario JOIN resposta 
+	ON usuario.idUsuario = resposta.fkUsuario JOIN alternativa 
+    ON alternativa.idAlternativa = resposta.fkAlternativa
     WHERE fkPesquisa = 3
-	GROUP BY resposta.r_alternativa;
+	GROUP BY alternativa.caractere;
     
 -- PERGUNTA 3
-SELECT resposta.r_alternativa, COUNT(alternativa.fkResposta) FROM usuario JOIN alternativa 
-	ON usuario.idUsuario = alternativa.fkUsuario JOIN resposta 
-    ON resposta.idResposta = alternativa.fkResposta
+SELECT alternativa.caractere, COUNT(resposta.fkAlternativa) FROM usuario JOIN resposta 
+	ON usuario.idUsuario = resposta.fkUsuario JOIN alternativa 
+    ON alternativa.idAlternativa = resposta.fkAlternativa
     WHERE fkPesquisa = 4
-	GROUP BY resposta.r_alternativa;
+	GROUP BY alternativa.caractere;
 
-SELECT * FROM pesquisa JOIN resposta ON fkPesquisa = idPesquisa
-JOIN alternativa ON fkResposta = idResposta;
+SELECT * FROM pesquisa JOIN alternativa ON fkPesquisa = idPesquisa
+JOIN resposta ON fkAlternativa = idAlternativa;
 
-SELECT * FROM resposta JOIN alternativa ON fkResposta = idResposta;
+SELECT * FROM alternativa JOIN resposta ON fkAlternativa = idAlternativa;
 
 -- TOTAL DE RESPOSTAS EM CADA PERGUNTA
 SELECT COUNT(idAlternativa) 'qtd'
